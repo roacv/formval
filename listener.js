ae38b2842a57e693221aa440ae62f29f17dd61cd
roacv <roacv@hotmail.com>
2022-12-07 13:48:05 +0000

    Version final

--- listener.js
+++ listener.js
@@ -1,13 +1,15 @@
 let btn = document.querySelector('.card-footer');
-
 btn.addEventListener('click', function(evento){
-    //console.log(evento);
+    let inpt = document.getElementsByTagName('input');
+    let sele = document.getElementById('state');
+    // es -4 por los check de las tarjetas para no revisarlos
+    let alert=false;
+    for (let i=0; i<inpt.length-4; i++){
+        if (inpt[i].value==""){
+            alert=true;
+        }
+    }
+    console.log(alert);
     var element = document.getElementsByClassName("alert");
-    console.log(element[0].attributes[0]);
-    element[0].classList.remove('d-none');
-
-    //let front = document.getElementsByClassName('front');
-    //front[0].getElementsByTagName('li');
-    //front[0].classList.add('frontend');
-    //front[0].classList.remove('front');
-})
+    alert ? element[0].classList.remove('d-none') : element[0].classList.add('d-none')
+ })
