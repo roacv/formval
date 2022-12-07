f581121ecd464ff6d60312a878d4ad89c3ab5bf8
roacv <roacv@hotmail.com>
2022-12-07 14:49:12 +0000

    Final corregido

--- listener.js
+++ listener.js
@@ -7,9 +7,24 @@ btn.addEventListener('click', function(evento){
     for (let i=0; i<inpt.length-4; i++){
         if (inpt[i].value==""){
             alert=true;
+            inpt[i].classList.add('bg-warning');
+            inpt[i].classList.add('bg-gradient');
+        }else{
+            inpt[i].classList.remove('bg-warning');
+            inpt[i].classList.remove('bg-gradient');
         }
     }
-    console.log(alert);
+    if(sele.value==0){
+        sele.classList.add('bg-warning')
+        sele.classList.add('bg-gradient');
+        alert=true; 
+        evento.preventDefault();
+    }else{
+        sele.classList.remove('bg-warning')
+        sele.classList.remove('bg-gradient');
+        alert=false;
+    } 
+    console.log(sele.value);
     var element = document.getElementsByClassName("alert");
     alert ? element[0].classList.remove('d-none') : element[0].classList.add('d-none')
  })
